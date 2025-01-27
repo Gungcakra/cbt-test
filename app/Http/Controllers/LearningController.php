f4aa4b1f918cd2c2a75fbb91dffa5edc5f71097d
Gungcakra <agungcakra888@gmail.com>
2025-01-27 12:04:08 +0800

    Add learning finished view and controller method for course completion

--- app/Http/Controllers/LearningController.php
+++ app/Http/Controllers/LearningController.php
@@ -60,7 +60,12 @@ class LearningController extends Controller
         ]);
     }
 
-
+    public function learning_finished(Course $course)
+    {
+        return view('student.courses.learning_finished',[
+            'course' => $course
+        ]);
+    }
     public function learning_rapport(Course $course)
     {
 
